7192a992b0189b3c8753f5d30f94370b580be32e
Chamali Wedage <94621140+ChamaliWedage-99@users.noreply.github.com>
2023-12-16 15:12:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the To-Do App</h1><To
+      <h1>Welcome to the To-Do App</h1><Todo/>
     </div>
   );
 }
